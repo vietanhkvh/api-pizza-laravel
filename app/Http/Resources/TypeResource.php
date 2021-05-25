09a4c781ee5ef3b5ef821a402219564b2305170d
DumToryo <83193243+thiendum2020@users.noreply.github.com>
2021-05-25 08:49:43 +0700

    z

--- app/Http/Resources/TypeResource.php
+++ app/Http/Resources/TypeResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class PostResource extends JsonResource
+class TypeResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
