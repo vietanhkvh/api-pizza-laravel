9c7614d58d6f2bf42cab332cff47d7be756b1897
DumToryo <83193243+thiendum2020@users.noreply.github.com>
2021-05-25 08:37:34 +0700

    a

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\ProductController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
