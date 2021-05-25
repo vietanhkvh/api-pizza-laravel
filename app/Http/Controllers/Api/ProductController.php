450a8562eefe2011ef6b611c57835dbd1314b2ca
DumToryo <83193243+thiendum2020@users.noreply.github.com>
2021-05-25 10:39:52 +0700

    x

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -89,8 +89,11 @@ class ProductController extends Controller
      */
     public function show($id)
     {
+        
         $product = ProductModel::where(['id' => $id])->get();
-
+        if(is_null($product)){
+            return response()->json(["message"=>"Record not found!"], 404);
+        }
         return response()->json(['status' => 1, 'data' => ProductResource::collection($product)], 201);
     }
 
@@ -114,7 +117,35 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $product = ProductModel::where(['id' => $id]);
+        if(is_null($product)){
+            return response()->json(["message"=>"Record not found!"], 404);
+        }
+        $product->update($request->all());
+
+        $file = $request->file('image');
+        $resource = fopen($file, "r") or die("File upload Problems");
+
+        // Upload hinh anh len Imgur bang API
+        $imgur_client = new Client(['base_uri' => 'https://api.imgur.com/3/upload']);
+        $imgur_response = $imgur_client->post('image', [
+            'headers' => [
+                'Authorization' => 'Client-ID 46dded22984842f',
+
+            ],
+            'multipart' => [
+                [
+                    'Content-Type' => 'multipart/form-data; boundary=<calculated when request is sent>',
+                    'name' => 'image',
+                    'contents' => $resource,
+                ]
+            ]
+        ]);
+
+        $img_link = json_decode($imgur_response->getBody())->data->link;
+        ProductModel::where(['id' => $id])->update(['image' => $img_link]);
+
+        return response()->json(['status' => 1, 'data' => ProductResource::collection(ProductModel::all())], 200);
     }
 
     /**
@@ -126,6 +157,11 @@ class ProductController extends Controller
     public function destroy($id)
     {
         $product = ProductModel::where(['id' => $id])->first();
+        if(is_null($product)){
+            return response()->json(["message"=>"Record not found!"], 404);
+        }else{
+             $product->delete();
+        }
         $product->delete();
 
         return response()->json(['status' => 1, 'data' => null], 404);
--- routes/api.php
+++ routes/api.php
@@ -54,4 +54,5 @@ Route::get('product', [ProductController::class, 'index']);
 Route::get('product/type/{typeid}', [ProductController::class, 'getProductByTypeId']);
 Route::get('product/{id}', [ProductController::class, 'show']);
 Route::post('product', [ProductController::class, 'store']);
+Route::put('product/{id}', [ProductController::class, 'update']);
 Route::delete('product/{id}', [ProductController::class, 'destroy']);
